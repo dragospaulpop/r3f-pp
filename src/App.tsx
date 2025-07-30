61f3b0fcf208332039a580c199727d17b424e27c
Dragos-Paul Pop <froind@gmail.com>
2025-07-30 17:28:35 +0300

    <Select> fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,25 @@ import {
   PerspectiveCamera,
   useGLTF,
 } from "@react-three/drei";
-import { Canvas, type ThreeEvent } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import {
   EffectComposer,
   Outline,
-  Selection,
   selectionContext,
   type SelectApi,
 } from "@react-three/postprocessing";
-import { useContext, useEffect, useRef, useState } from "react";
+import { memo, useContext, useEffect, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
 
 import "./App.css";
 
 function App() {
-  const [isSelected, setIsSelected] = useState(false);
+  const [selectedId, setSelectedId] = useState<string | null>(null);
 
   return (
     <div className="w-screen h-screen bg-neutral-300">
-      <Canvas onPointerMissed={() => setIsSelected(false)}>
-        <OrbitControls enabled={!isSelected} />
+      <Canvas onPointerMissed={() => setSelectedId(null)}>
+        <OrbitControls enabled={!selectedId} />
         <PerspectiveCamera makeDefault position={[0, 0, 10]} />
         <ambientLight intensity={Math.PI / 2} />
         <spotLight
@@ -46,7 +45,42 @@ function App() {
               hiddenEdgeColor={0x22090a}
             />
           </EffectComposer>
-          <CustomBox isSelected={isSelected} setIsSelected={setIsSelected} />
+          <CustomBox
+            isSelected={selectedId === "a"}
+            onPointerDown={() => setSelectedId("a")}
+            position={[0, 0, 0]}
+            color="red"
+          />
+          <CustomBox
+            isSelected={selectedId === "b"}
+            onPointerDown={() => setSelectedId("b")}
+            position={[0, 1, 0]}
+            color="blue"
+          />
+          <CustomBox
+            isSelected={selectedId === "c"}
+            onPointerDown={() => setSelectedId("c")}
+            position={[0, 2, 0]}
+            color="green"
+          />
+          <CustomBox
+            isSelected={selectedId === "d"}
+            onPointerDown={() => setSelectedId("d")}
+            position={[0, 3, 0]}
+            color="yellow"
+          />
+          <CustomBox
+            isSelected={selectedId === "e"}
+            onPointerDown={() => setSelectedId("e")}
+            position={[0, 4, 0]}
+            color="purple"
+          />
+          <CustomBox
+            isSelected={selectedId === "f"}
+            onPointerDown={() => setSelectedId("f")}
+            position={[0, 5, 0]}
+            color="orange"
+          />
         </Selection>
 
         <Table />
@@ -72,33 +106,52 @@ function Table() {
   );
 }
 
-function CustomBox({
-  isSelected,
-  setIsSelected,
-}: {
-  isSelected: boolean;
-  setIsSelected: (selected: boolean) => void;
-}) {
-  const handleClick = (e: ThreeEvent<MouseEvent>) => {
-    e.stopPropagation();
-    setIsSelected(true);
-  };
+const CustomBox = memo(
+  ({
+    isSelected,
+    onPointerDown,
+    color,
+    position,
+  }: {
+    isSelected: boolean;
+    onPointerDown: () => void;
+    color: string;
+    position: [number, number, number];
+  }) => {
+    return (
+      <Select enabled={isSelected}>
+        <DragControls axisLock={"y"}>
+          <group scale={1} position={position} onClick={onPointerDown}>
+            <mesh>
+              <boxGeometry args={[1, 1, 1]} />
+              <meshStandardMaterial color={color} />
+            </mesh>
+          </group>
+        </DragControls>
+      </Select>
+    );
+  },
+  (prevProps, nextProps) => {
+    return prevProps.isSelected === nextProps.isSelected;
+  }
+);
 
+export function Selection({
+  children,
+  enabled = true,
+}: {
+  enabled?: boolean;
+  children: React.ReactNode;
+}) {
+  const [selected, select] = useState<THREE.Object3D[]>([]);
+  const value = useMemo(
+    () => ({ selected, select, enabled }),
+    [selected, enabled]
+  );
   return (
-    <Select enabled={isSelected}>
-      <DragControls axisLock={"y"}>
-        <mesh scale={1} position={[0, 0, 0]} onClick={handleClick}>
-          <Box
-            position={[0, 0.55, 0]}
-            rotation={[-Math.PI / 2, 0, 0]}
-            scale={1}
-            args={[1, 1, 1]}
-          >
-            <meshStandardMaterial color="hotpink" />
-          </Box>
-        </mesh>
-      </DragControls>
-    </Select>
+    <selectionContext.Provider value={value}>
+      {children}
+    </selectionContext.Provider>
   );
 }
 
@@ -109,20 +162,57 @@ export function Select({ enabled = false, children, ...props }: SelectApi) {
     if (api && enabled) {
       let changed = false;
       const current: THREE.Object3D[] = [];
+
       group.current.traverse((o) => {
-        o.type === "Mesh" && current.push(o);
-        if (api.selected.indexOf(o) === -1) changed = true;
+        if (o.type === "Mesh") {
+          current.push(o);
+          if (!api.selected.some((m) => m.uuid === o.uuid)) changed = true;
+        }
       });
+
+      console.log("------RUN------");
+
+      console.log(
+        "API.SELECTED: ",
+        JSON.stringify(api.selected.map((m) => m.uuid))
+      );
+
+      console.log("CHANGED: ", changed);
+
+      console.log("CURRENT: ", JSON.stringify(current.map((m) => m.uuid)));
+
       if (changed) {
-        api.select((state) => [...state, ...current]);
-        return () => {
-          api.select((state) =>
-            state.filter((selected) => !current.includes(selected))
+        api.select((state) => {
+          console.log(
+            "adding: ",
+            `STATE: ${JSON.stringify(state.map((m) => m.uuid))}`,
+            `CURRENT: ${JSON.stringify(current.map((m) => m.uuid))}`
           );
+          return [...state, ...current];
+        });
+        console.log(
+          "changed API.SELECTED: ",
+          JSON.stringify(api.selected.map((m) => m.uuid))
+        );
+        return () => {
+          api.select((state) => {
+            console.log(
+              "cleanup STATE:",
+              JSON.stringify(state.map((m) => m.uuid))
+            );
+            return state.filter((selected) => {
+              console.log(
+                "filtering: ",
+                `SELECTED: ${selected.uuid}`,
+                `CURRENT: ${JSON.stringify(current.map((m) => m.uuid))}`
+              );
+              return current.map((m) => m.uuid).includes(selected.uuid);
+            });
+          });
         };
       }
     }
-  }, [enabled, children]);
+  }, [enabled, children, api]);
   return (
     <group ref={group} {...props}>
       {children}
